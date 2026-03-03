abe6398c0851406d79418b906766759baf43320e
aa-Teye <alexteyeametepey@gmail.com>
2026-03-03 13:35:50 +0000

    Working on Annotations

---
+++ ANNOTATIONS.py
@@ -0,0 +1,52 @@
+# from typing import List
+
+# def get_full_name(first_name, last_name):
+#     full_name =  first_name.title() + " " + last_name.title()
+#     return full_name
+
+
+# print(get_full_name("Alex","Ametepey"))
+
+#TRYING WIHT TYPE HINT: This makes the work easier by helping the autocomplete provide more suggestions with what we are doing.
+# def get_full_name2(firstname2: str, last_name2: str):
+#     full_name2 = firstname2.title() + " " + last_name2.title()
+#     return full_name2
+
+# print(get_full_name2("John","Pinto"))
+
+#USING IT FOR DATA TYPES   HAVING CHALLENGES WITH THIS CODE WILL COME BACK TO IT 
+# def process_items(items: list[str]):
+#     for item in items:
+#         return item.title()
+
+# print(process_items(["waakye", "fish", "egg stew","rice"]))
+
+# def process_items(prices: dict[str,float]):
+#     for item_name, item_price in prices.items():
+#         print(item_name)
+#         print(item_price)
+
+# print(process_items({"food" : 27.5, "rent": 14000
+
+# }))
+
+#Using the Union 
+# def process_item(item: int | str):
+#     return item.title()
+
+# print(process_item("halidu"))
+
+
+# def say_hi(name: str | None):
+#     if name is not None:
+#         return(f"Hey i am {name}")
+#     else:
+#         return("hello world")
+       
+# message = say_hi("alidu")
+# print(message.title())
+
+def say_hello(name: str):
+    print(f"hello, my name is {name}.How are you doing?")
+    return name.title()
+
