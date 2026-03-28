e0bfbb421f8d25208f50ac5697c7a8a860b0d0f4
aa-Teye <alexteyeametepey@gmail.com>
2026-03-28 23:53:14 +0000

    updating main.py file

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ def get_item(item_name: str):
         raise HTTPException(status_code=404, detail="Gear not found")
     return item
 
+# 4. CREATE: Add new gear via the web
+@app.post("/inventory/add")
+def add_new_gear(name: str, qty: int, status: str = "Operational"):
+    manager.add_item(name, qty, status)
+    return {"message": f"Successfully added {name}", "data": manager.vault[name]}
