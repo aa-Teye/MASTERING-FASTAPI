25fd57418e5908eb07ad5f3474c89d7ebd58b45e
aa-Teye <alexteyeametepey@gmail.com>
2026-04-04 23:34:48 +0000

    Initializing he notifier.py file for the head of media notification permit

--- auth.py
+++ auth.py
@@ -12,7 +12,7 @@ def verify_admin(api_key: str = Security(api_key_header)):
     Dependency function: Checks if the user provided the correct admin password.
     If they fail, it blocks them with a 401 Unauthorized error.
     """
-    if api_key != ADMIN_SECRET_KEY:
+    if api_key != ADMIN_SECRET_KEY;
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Access Denied: Invalid Church Admin Key",
